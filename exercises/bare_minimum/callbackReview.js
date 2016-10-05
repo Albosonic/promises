30e3662ae58e4f16ff42c7f7d013ace7e177fbd3
El_Gato <albertomadueno@gmail.com>
2016-10-05 12:27:40 -0700

    bare minimum/callbackReview complete

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -10,9 +10,8 @@ var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
   fs.readFile(filePath, function(err, data) {
     if (err) {
-
       callback(err);
-    }
+    }    
     var line = data.toString();
     line = line.slice(0, 22);
     callback(null, line);
@@ -22,6 +21,13 @@ var pluckFirstLineFromFile = function (filePath, callback) {
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
   // TODO
+  request(url, function (err, response, body) {
+    if (!err && response.statusCode == 200) {
+      callback(null, response.statusCode)
+    } else {
+      callback(err);
+    }
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
